996379b12687d2794e0247ae1b28bf7020d0a84b
PC <lafouinedjamel@gmail.com>
2024-09-02 19:01:19 +0100

    P1c2d2 OK

--- src/com/zerofiltre/parkingbot/Main.java
+++ src/com/zerofiltre/parkingbot/Main.java
@@ -3,6 +3,13 @@ package com.zerofiltre.parkingbot;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int lenght = 120;
+        int width = 100;
+        int surface = lenght * width;
+        int perimeter = (lenght + width) * 2;
+        System.out.println(perimeter);
+        System.out.println(surface);
+
+       
     }
 }
